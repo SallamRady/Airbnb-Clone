f952a465fd0dcc574c0b8c8cf26e372f850e361b
SallamRady <sallamrady@gmail.com>
2023-05-17 21:01:33 +0300

    Set up Front-end and Create main header component

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import Layout from "./pages/Layout";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>
+        Life is Hard man and there is alot of chanllenges you should grow or u
+        will die.
+      </h1>
+    </>
+  );
+};
+
+export default Home;
